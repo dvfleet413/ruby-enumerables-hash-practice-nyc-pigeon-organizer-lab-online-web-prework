b06a6cdac3d45b7a9c273b666b615f1854778c29
David Van Fleet <dvfleet413@gmail.com>
2019-12-22 01:30:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  # Iterate through entire hash to find names of
+  # pigeons. If pigeon hash key doesn't exist, add
+  # it to the new hash
+  
+
+  sorted_names = []
+  data[:color].each_value{|value| sorted_names << value}
+  data[:gender].each_value{|value| sorted_names << value}
+  data[:lives].each_value{|value| sorted_names << value}
+  sorted_names.flatten!
+  sorted_names.uniq!
+  sorted_names.sort!
+  
+
+  result = []
+  i = 0 
+  while i < sorted_names.length do
+    name = sorted_names[i]
+    result[name] = {}
+    i += 1 
+  end
+  
+  # Iterate through source hash again, this time
+  # adding new keys to each pigeon hash with 
+  # appropriate values
+  
+  
+  
 end
